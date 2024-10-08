d39b81b71a104a4de341db173c0031696f161922
Jesal <jesalvadgama@gmail.com>
2024-10-08 10:22:46 +0100

    code: mediapipe initial setup with left,right hand tracking and detection

---
+++ data_collection.py
@@ -0,0 +1,28 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+
+offset = 20
+imgSize = 300
+
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img) # creates dots on hands
+    if hands:
+        hand = hands[0] # Having one hand
+        # Crop image for classification
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize,3),np.uint8)*255
+        #put image crop matrix inside image white matrix
+
+
+        imgCrop = img[y-offset:y + h+offset, x-offset:x + w+offset]
+        cv2.imshow("imageCrop", imgCrop)
+        cv2.imshow("imageWhite", imgWhite)
+
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
