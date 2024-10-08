4e4ffbd2eb0f70e58f522640c972987b2472e0a3
Jesal <jesalvadgama@gmail.com>
2024-10-08 23:12:09 +0100

    documentation: updated data with a,b,c,d,e and trained imgWhite to identify these gestures, customised UI with letters to show which hand sign it is

--- data_collection.py
+++ data_collection.py
@@ -2,12 +2,16 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 import math
+import time
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
 
 offset = 20
 imgSize = 300
+
+folder = "data/left/el"
+counter =  0
 
 while True:
     success, img = cap.read()
@@ -47,4 +51,8 @@ while True:
         cv2.imshow("imageWhite", imgWhite)
 
     cv2.imshow("Image",img)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    if key == ord("s"):
+        counter +=1
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
+        print(counter)
