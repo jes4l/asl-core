0c1a447ab87366ee4feed40139754bedd2cccf34
Jesal <jesalvadgama@gmail.com>
2024-10-08 11:30:52 +0100

    code: adjusted image of hand height, width and scales automatically to fit in screen

--- data_collection.py
+++ data_collection.py
@@ -1,6 +1,7 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
+import math
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
@@ -21,8 +22,25 @@ while True:
 
         imgCropShape = imgCrop.shape
 
-        # put image crop matrix inside image white matrix
-        imgWhite[0:imgCropShape[0],0:imgCropShape[1]] = imgCrop
+        aspectRatio = h/w
+
+        if aspectRatio >1:
+            k = imgSize/h
+            wCal = math.ceil(k * w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize - wCal)/2)
+            # put image crop matrix inside image white matrix
+            imgWhite[:, wGap:wCal+wGap] = imgResize
+
+        else:
+            k = imgSize/w
+            hCal = math.ceil(k * h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal)/2)
+            # put image crop matrix inside image white matrix
+            imgWhite[hGap:hCal+hGap] = imgResize
 
 
         cv2.imshow("imageCrop", imgCrop)
